cb99c5de77a4ba4dfa7cc0c80baec8a129f9df3b
Elixer <everett.rumery@gmail.com>
2024-09-17 13:54:54 -0400

    loops

--- main.java
+++ main.java
@@ -30,6 +30,11 @@ public class main {
             phone = input.nextLine();
         }while(phone.length()!= 10); 
 
+        for(int i = 0; i < 5; i++) {
+            for ( int j = 0; j < 5; j++) {
+                System.out.println(i+j);
+            }
+        }
 
     }
 }
